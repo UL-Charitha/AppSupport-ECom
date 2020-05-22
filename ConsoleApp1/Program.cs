92f7fa99f38d3942e88cec895fec82d5bd253938
Charitha Waravita <18893@srilankan.com>
2020-05-22 16:15:12 +0530

    ok.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,39 +12,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            DoWork();
+            Processor proc = new Processor();
+
+            proc.DoWork();
+
+            //var response = proc.ReadOutlookMsg();
         }
 
-        private static void DoWork()
-        {
-            var App = new Microsoft.Office.Interop.Outlook.Application();
+        //private static void DoWork()
+        //{
+        //    var App = new Microsoft.Office.Interop.Outlook.Application();
+        //    MailItem mailItem = App.CreateItem(OlItemType.olMailItem);
+        //    mailItem.Subject = "Hi test charitha";
+        //    mailItem.To = "wter@lkk.lk";
+        //    mailItem.CC = "cclol@lkk.lk";
+        //    mailItem.BCC = "bccm@lkkk.lk";
+        //    mailItem.Body = "Hi this is the body of the msg"; 
 
-            MailItem mailItem = App.CreateItem(OlItemType.olMailItem);
-
-            mailItem.Subject = "Hi test charitha";
-            mailItem.To = "wter@lkk.lk";
-            mailItem.CC = "cclol@lkk.lk";
-            mailItem.BCC = "bccm@lkkk.lk";
-            mailItem.Body = "Hi this is the body of the msg";
-
-            
-
-            string FileAtachment = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\pic1.jpg";
-            string msgSavePath = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\msgOut.msg";
-
-            // make sure a filename was passed
-            if (string.IsNullOrEmpty(FileAtachment) == false)
-            {
-                // need to check to see if file exists before we attach !
-                if (!File.Exists(FileAtachment))
-                    Console.WriteLine("Attached document " + FileAtachment + " does not exist", "File Error");
-                else
-                {
-                    Attachment attachment = mailItem.Attachments.Add(FileAtachment, Microsoft.Office.Interop.Outlook.OlAttachmentType.olByValue, Type.Missing, Type.Missing);
-                }
-            }
-            mailItem.Display();     // display the email
-            mailItem.SaveAs(msgSavePath, Type.Missing);
-        }
+        //    string FileAtachment = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\pic1.jpg";
+        //    string msgSavePath = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\msgOut.msg";
+        //    // make sure a filename was passed
+        //    if (string.IsNullOrEmpty(FileAtachment) == false)
+        //    {
+        //        // need to check to see if file exists before we attach !
+        //        if (!File.Exists(FileAtachment))
+        //            Console.WriteLine("Attached document " + FileAtachment + " does not exist", "File Error");
+        //        else
+        //        {
+        //            Attachment attachment = mailItem.Attachments.Add(FileAtachment, Microsoft.Office.Interop.Outlook.OlAttachmentType.olByValue, Type.Missing, Type.Missing);
+        //        }
+        //    }
+        //    mailItem.Display();     // display the email
+        //    mailItem.SaveAs(msgSavePath, Type.Missing);
+        //}
     }
 }
