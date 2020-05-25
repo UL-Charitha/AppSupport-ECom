105c9f22543b665dbe58bf1d40a92ae729f0bee0
Charitha Waravita <18893@srilankan.com>
2020-05-25 16:47:15 +0530

    ok..

---
+++ ConsoleApp1/MSGReader.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MSGReader
+    {
+        internal string ReadMsg(string path)
+        {
+			try
+			{
+				var App = new Microsoft.Office.Interop.Outlook.Application();
+				//var xxxx = App.Session.GetItemFromID("");
+				var mailItem = (MailItem) App.Session.OpenSharedItem(path); 
+				//as Microsoft.Office.Interop.Outlook.MailItem;
+				//string body = mailx.HTMLBody;
+				//int att = mailx.Attachments.Count;
+				var entryId = mailItem.EntryID;
+				mailItem.Display();
+				//var idx = App.Session.GetItemFromID("1");
+				return entryId;
+			}
+			catch (System.Exception)
+			{
+				throw;
+			}
+        }
+
+		internal string GetEntryIdByMsgFile(string path)
+		{
+			try
+			{
+				var App = new Microsoft.Office.Interop.Outlook.Application();
+				var mailItem = (MailItem)App.Session.OpenSharedItem(path);
+				mailItem.Display();
+				var entryID = mailItem.EntryID;
+				mailItem = null;
+				return entryID;
+			}
+			catch (System.Exception)
+			{
+				throw;
+			}
+		}
+
+		internal void ReadMsgByEntryId(string id)
+		{
+			var App = new Microsoft.Office.Interop.Outlook.Application();
+			var NS = App.GetNamespace("mapi");
+			
+			// Oopen mail by EntryID
+			var mailItem = (MailItem)NS.GetItemFromID(id);
+			mailItem.Display();
+		}
+
+		internal void ReadMsgRealTime(string path)
+		{
+			try
+			{
+				var App = new Microsoft.Office.Interop.Outlook.Application();
+				// Get the MAPI namespace.
+				var NS = App.GetNamespace("mapi");
+				//Get the Inbox folder.
+				MAPIFolder oInbox = NS.GetDefaultFolder(Microsoft.Office.Interop.Outlook.OlDefaultFolders.olFolderInbox);
+
+				
+
+				//Get the Items collection in the Inbox folder.
+				Items inboxItems = oInbox.Items;
+				var count = inboxItems.Count;
+
+				// Get the first message.
+				MailItem Msg1 = (MailItem)inboxItems.GetFirst();
+
+				MailItem Msg2 = (MailItem)inboxItems.GetLast();
+				Console.WriteLine(Msg2.ConversationID);
+				var tempx = Msg2.ConversationTopic;
+				Console.WriteLine(Msg2.EntryID);
+				Console.WriteLine(Msg2.ReceivedTime);
+				Console.ReadLine();
+				//Msg2.Display();
+			}
+			catch (System.Exception ex)
+			{
+				var xxx = ex.Message;
+				// https://support.microsoft.com/en-sg/help/310258/how-to-use-the-microsoft-outlook-object-library-to-retrieve-a-message
+			}
+		}
+	}
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,39 +12,56 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Processor proc = new Processor();
+            try
+            {
+                string path = @"D:\Projects\CSP\Test\YourWeeklyAttendance.msg";
+                string entryIdFix = "000000003612B23E363C234DB1668719ADC28ED80700D0B65853BEFAB740A79FC94F998287A6000000B860FB00008518C8EE6B02CC47A40E72278D44F18C00047A6F067A0000";
+                Processor proc = new Processor();
+                MSGReader msgReader = new MSGReader();
 
-            //proc.DoWork();
-            proc.TestSentBox(null);
+                string entryId = msgReader.GetEntryIdByMsgFile(path);
+                msgReader.ReadMsgByEntryId(entryIdFix);
 
-            //var response = proc.ReadOutlookMsg();
+                //msgReader.ReadMsgRealTime("");
+                //msgReader.ReadMsg(path);
+
+                //proc.DoWork();
+                //proc.TestSentBox(null);
+
+                //var response = proc.ReadOutlookMsg();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
         }
 
-        //private static void DoWork()
-        //{
-        //    var App = new Microsoft.Office.Interop.Outlook.Application();
-        //    MailItem mailItem = App.CreateItem(OlItemType.olMailItem);
-        //    mailItem.Subject = "Hi test charitha";
-        //    mailItem.To = "wter@lkk.lk";
-        //    mailItem.CC = "cclol@lkk.lk";
-        //    mailItem.BCC = "bccm@lkkk.lk";
-        //    mailItem.Body = "Hi this is the body of the msg"; 
+        private static void DoWorkLater()
+        {
+            var App = new Microsoft.Office.Interop.Outlook.Application();
+            MailItem mailItem = App.CreateItem(OlItemType.olMailItem);
+            mailItem.Subject = "Hi test charitha";
+            mailItem.To = "wter@lkk.lk";
+            mailItem.CC = "cclol@lkk.lk";
+            mailItem.BCC = "bccm@lkkk.lk";
+            mailItem.Body = "Hi this is the body of the msg";
 
-        //    string FileAtachment = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\pic1.jpg";
-        //    string msgSavePath = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\msgOut.msg";
-        //    // make sure a filename was passed
-        //    if (string.IsNullOrEmpty(FileAtachment) == false)
-        //    {
-        //        // need to check to see if file exists before we attach !
-        //        if (!File.Exists(FileAtachment))
-        //            Console.WriteLine("Attached document " + FileAtachment + " does not exist", "File Error");
-        //        else
-        //        {
-        //            Attachment attachment = mailItem.Attachments.Add(FileAtachment, Microsoft.Office.Interop.Outlook.OlAttachmentType.olByValue, Type.Missing, Type.Missing);
-        //        }
-        //    }
-        //    mailItem.Display();     // display the email
-        //    mailItem.SaveAs(msgSavePath, Type.Missing);
-        //}
+            string FileAtachment = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\pic1.jpg";
+            string msgSavePath = @"D:\Projects\CSP\Repo.RnD\ConsoleCSP\ConsoleApp1\msgOut.msg";
+            // make sure a filename was passed
+            if (string.IsNullOrEmpty(FileAtachment) == false)
+            {
+                // need to check to see if file exists before we attach !
+                if (!File.Exists(FileAtachment))
+                    Console.WriteLine("Attached document " + FileAtachment + " does not exist", "File Error");
+                else
+                {
+                    Attachment attachment = mailItem.Attachments.Add(FileAtachment, Microsoft.Office.Interop.Outlook.OlAttachmentType.olByValue, Type.Missing, Type.Missing);
+                }
+            }
+            mailItem.Display();     // display the email
+            mailItem.SaveAs(msgSavePath, Type.Missing);
+        }
     }
 }
